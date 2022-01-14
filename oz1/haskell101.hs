9235eb87b54a5b5b748c5cd773f4920ac9e37a53
Yori Verbist <yori.verbist1212@gmail.com>
2022-01-14 14:43:39 +0100

    Prep 1.1 done

---
+++ oz1/haskell101.hs
@@ -0,0 +1,19 @@
+module Template where
+
+double :: Int -> Int
+double x = x * 2
+
+myAbs :: Int -> Int
+myAbs x
+    | x > 0 = x
+    | otherwise = -x
+
+toFahrenheit :: Float -> Float
+toFahrenheit x = 1.8 * x + 32
+
+fizzbuzz :: Int -> String
+fizzbuzz x
+    | mod x 3 == 0 && mod x 5 == 0 = "fizzbuzz"
+    | mod x 3 == 0 = "fizz"
+    | mod x 5 == 0 = "buzz"
+    | otherwise = show x

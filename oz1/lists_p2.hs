7f38142eed73eb6bd68fc6d151a8c40a2ed0b8af
Yori Verbist <yori.verbist1212@gmail.com>
2022-12-25 13:21:52 +0100

    update

---
+++ oz1/lists_p2.hs
@@ -0,0 +1,10 @@
+module Template where
+
+myProduct :: [Integer] -> Integer
+myProduct = error "not implemented"
+
+insert :: Int -> [Int] -> [Int]
+insert = error "not implemented"
+
+myLast :: [Int] -> Int
+myLast = error "Not implemented"

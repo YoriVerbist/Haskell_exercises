5ca936212f192c601ae22ede8160ffb74ed36f4c
Yori Verbist <yori.verbist12@gmail.com>
2023-08-03 11:37:24 +0200

    finished 1.4

--- oz1/list_comprehensions.hs
+++ oz1/list_comprehensions.hs
@@ -20,3 +20,9 @@ sumInts low high = sum [low..high]
 
 removeMultiples :: Int -> [Int] -> [Int]
 removeMultiples num l = [x | x <- l, x `mod` num /= 0]
+
+mapLC :: (a -> b) -> [a] -> [b]
+mapLC f l = [f x | x <- l]
+
+filterLC :: (a -> Bool) -> [a] -> [a]
+filterLC f l = [x | x <- l, f x]
